84fcce1223dbb86ee18ebe574670b99ee41a1d3b
simondumalski <simon@dumalski.com>
2023-01-31 15:40:15 -0700

    Added a function for getting a master week

--- server/src/controllers/weekController.js
+++ server/src/controllers/weekController.js
@@ -1,3 +1,6 @@
+import mongoose from 'mongoose';
+import Week from '../models/Week.js';
+
 //GET a single week
 export const getWeek = async (req, res) => {
     //The ID of the week to get
@@ -9,9 +12,9 @@ export const getWeek = async (req, res) => {
     }
 
     //Find the week
-    let week = await Week.findOne({ _id: id });
+    const week = await Week.findOne({ _id: id });
 
-    //If no week is returned, respnod with an error
+    //If no week is returned, respond with an error
     if (!week) {
         res.status(404).json({ error: 'No such week' });
     } else {
@@ -20,6 +23,20 @@ export const getWeek = async (req, res) => {
 };
 
 //CREATE a new master week
+export const getMasterWeek = async (req, res) => {
+    //The name of the week to get
+    const { name } = req.params;
+
+    //Find the week
+    const week = await Week.findOne({ name, is_master: true });
+
+    //If no week is returned, respond with an error
+    if (!week) {
+        res.status(404).json({ error: 'No such master week' });
+    } else {
+        res.status(200).json({ week });
+    }
+};
 
 //CREATE a new week based on a master week
 
