29d6834c8aa0c415ed06b6a95496a123ebcf6398
simondumalski <simon@dumalski.com>
2023-01-31 12:31:25 -0700

    Added the routes to the index.js

--- server/index.js
+++ server/index.js
@@ -8,22 +8,24 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 // import middleware
-import errorHandler from './src/middleware/errorHandler.js'
+import errorHandler from './src/middleware/errorHandler.js';
 
 // configure environemnt variables
 dotenv.config({ path: './.env' });
 
 // import routers
+import weekRoutes from './src/routes/weekRoutes.js';
 
 // create an instance of express (i think)
 const app = express();
 
 // initialize middleware
-app.use(express.json());    // enables express to parse json payloads
-app.use(cors());            // enables cross origin requests 
+app.use(express.json()); // enables express to parse json payloads
+app.use(cors()); // enables cross origin requests
 
 // initialize routers
 // app.use(yourRouterHere);
+app.use('/api/weeks', weekRoutes);
 
 // initialize error handling middleware
 app.use(errorHandler);
@@ -31,13 +33,14 @@ app.use(errorHandler);
 // hello, world 👋
 app.get('/', (request, response) => {
     response.status(200).json({
-        message: 'a poison to erase my existance!'
+        message: 'a poison to erase my existance!',
     });
 });
 
 // 🐸
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGOOSE_URI)
+mongoose
+    .connect(process.env.MONGOOSE_URI)
     .then((connection) => {
         app.listen(process.env.PORT, () => {
             console.log('database connection successful!');
