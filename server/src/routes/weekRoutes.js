526886bbc92f47dcb6d22ee24be5315cab1dc79d
simondumalski <simon@dumalski.com>
2023-01-31 15:12:49 -0700

    Added the basics for each route we will need

--- server/src/routes/weekRoutes.js
+++ server/src/routes/weekRoutes.js
@@ -3,13 +3,18 @@ import express from 'express';
 const router = express.Router();
 
 //GET a single week
+router.get('/:id');
 
 //POST a new master week
+router.post('/master');
 
 //POST a new week based on a master week
+router.post('/:id');
 
 //PATCH a week
+router.patch('/:id');
 
 //DELETE a week
+router.delete('/:id');
 
 export default router;
