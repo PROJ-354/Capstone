c12bf61e3cb7f261a2de86fdb240bd3c6b6f7461
simondumalski <simon@dumalski.com>
2023-01-31 11:46:59 -0700

    Added a loader function

--- client/src/pages/student/checklist/ViewChecklist.jsx
+++ client/src/pages/student/checklist/ViewChecklist.jsx
@@ -1,5 +1,23 @@
 import { Box } from '@mui/material';
+import { useLoaderData } from 'react-router-dom';
 
 export default function ViewChecklist() {
+    //Grabs the data from the backend using the loader function
+    const checklist = useLoaderData();
+
     return <Box>ViewChecklist</Box>;
 }
+
+export const checklistLoader = async ({ params }) => {
+    //TODO: set this to the correct route
+    const res = await fetch(`http://localhost:42069/api/checklists/${params.id}`);
+
+    if (!res.ok) {
+        console.log('There was an error retreiving that checklist');
+    }
+
+    const data = await res.json();
+    return data;
+};
+
+export const checklistAction = () => {};
