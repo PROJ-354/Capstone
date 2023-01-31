56e301a9e1f3bb13ab34360ea14ee0626b891705
simondumalski <simon@dumalski.com>
2023-01-31 12:30:11 -0700

    Added comments for all the routes we will need

---
+++ server/src/routes/weekRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+//GET a single week
+
+//POST a new master week
+
+//POST a new week based on a master week
+
+//PATCH a week
+
+//DELETE a week
+
+export default router;
