8b3386c4adac652901db22bd4ef4370c488f55b7
Refaat0 <elhajj.refaat@gmail.com>
2023-01-31 15:01:24 -0700

    worked on the Schedule model

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import errorHandler from './src/middleware/errorHandler.js'
 dotenv.config({ path: './.env' });
 
 // import routers
+import WeekRouter from './src/routes/WeekRouter.js';
 
 // create an instance of express (i think)
 const app = express();
@@ -23,7 +24,7 @@ app.use(express.json());    // enables express to parse json payloads
 app.use(cors());            // enables cross origin requests 
 
 // initialize routers
-// app.use(yourRouterHere);
+app.use(WeekRouter);
 
 // initialize error handling middleware
 app.use(errorHandler);
