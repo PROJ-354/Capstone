e73e26d9cd8c69f89d83511ec634410c25d4f2e0
Refaat0 <elhajj.refaat@gmail.com>
2023-02-01 15:29:30 -0700

    halfbaked users system

---
+++ server/src/routes/UserRouter.js
@@ -0,0 +1,20 @@
+/**
+ * this module exports a Router that contains all the endpoints for the User resource
+ */
+import express from 'express';
+const router = express.Router();
+
+// import controller functions
+import {
+    getAllUsers, createUser, updateUser, deleteUser
+} from '../controllers/UserController.js';
+
+// import middleware functions
+// blah blah blah
+
+router.get('/api/users', getAllUsers);
+router.post('/api/users', createUser);
+router.put('/api/users/:id', updateUser);
+router.delete('/api/users/:id', deleteUser);
+
+export default router;
