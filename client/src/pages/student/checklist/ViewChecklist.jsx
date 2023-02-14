8ccd88a85fa418ebec91423b2e8e337e998b2ab8
simondumalski <simon@dumalski.com>
2023-02-14 12:43:22 -0700

    Checklists should now be displayed

--- client/src/pages/student/checklist/ViewChecklist.jsx
+++ client/src/pages/student/checklist/ViewChecklist.jsx
@@ -1,11 +1,45 @@
-import { Box } from '@mui/material';
-import { useLoaderData } from 'react-router-dom';
+import { Box, Tab } from '@mui/material';
+import { Form, useLoaderData } from 'react-router-dom';
+import { useState } from 'react';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { ChecklistTabPanel } from '../../../components/ChecklistTabPanel';
 
 export default function ViewChecklist() {
+    const [tabValue, setTabValue] = useState(null);
+
+    const handleChange = (event, newValue) => {
+        setTabValue(newValue);
+    };
+
     //Grabs the data from the backend using the loader function
     const checklist = useLoaderData();
 
-    return <Box>ViewChecklist</Box>;
+    return (
+        <Box>
+            <TabContext value={tabValue}>
+                <Box>
+                    <TabList aria-label="Skills assessment forms" onChange={handleChange}>
+                        {checklist.skills_assessment.sections.map((section) => (
+                            <Tab
+                                key={section.name}
+                                label={section.name}
+                                value={section.name}
+                            />
+                        ))}
+                    </TabList>
+                </Box>
+                <Form method="post">
+                    {checklist.skills_assessment.sections.map((section) => (
+                        <ChecklistTabPanel
+                            key={section.name}
+                            section={section}
+                            skills={section.skills}
+                        />
+                    ))}
+                </Form>
+            </TabContext>
+        </Box>
+    );
 }
 
 export const checklistLoader = async ({ params }) => {
