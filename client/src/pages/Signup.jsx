8ff368e536dd789b59153ef15dc834546b717d8b
simondumalski <simon@dumalski.com>
2023-03-07 20:05:20 -0700

    Removed a useless import from signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useSignup } from '../hooks/useSignup';
 import { useState } from 'react';
-import { Typography, Button, Stack, TextField, Link, Container } from '@mui/material';
+import { Typography, Button, Stack, TextField, Container } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
