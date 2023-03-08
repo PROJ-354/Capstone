8829a8b22d8e45924d6350d9359c0cad65601c99
simondumalski <simon@dumalski.com>
2023-03-07 19:14:07 -0700

    Navbar only shows if logged in now

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 export default function RootLayout() {
+    const loggedIn = !localStorage.getItem('auth') ? false : true;
+
     return (
         <Box>
-            <Navbar />
+            {loggedIn && <Navbar />}
             <Outlet />
         </Box>
     );
