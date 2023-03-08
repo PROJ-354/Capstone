d57c2070a6b624d16834eea7eaf5f3e5ae472f83
simondumalski <simon@dumalski.com>
2023-03-07 20:03:46 -0700

    Improved signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,8 @@
 import { useSignup } from '../hooks/useSignup';
 import { useState } from 'react';
-import { Typography, Button, Stack, TextField, Link } from '@mui/material';
-import Navbar from '../components/Navbar';
+import { Typography, Button, Stack, TextField, Link, Container } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 const Signup = () => {
     const [role, setRole] = useState('');
     const [sait_id, setID] = useState('');
@@ -12,6 +13,8 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const { signup, error, isLoading } = useSignup();
+
+    const navigate = useNavigate();
 
     /* Called by the sign up form onSubmit function.
      */
@@ -36,92 +39,95 @@ const Signup = () => {
      * Clicking the button fires the onSubmit function on the form.
      */
     return (
-        <>
-            {/* <Navbar></Navbar> */}
+        <Container maxWidth="sm" sx={{ padding: '20px' }}>
+            <Typography variant="h4" component="h1">
+                Competency Tracking Tool
+            </Typography>
             <form className="signup" onSubmit={handleLogin}>
-                <Typography variant="h4" component="h1">
-                    Competency Tracking Tool
-                </Typography>
                 <br />
                 <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold' }}>
-                    Create an account
+                    Sign up
                 </Typography>
                 <br />
                 <Stack spacing={2}>
-                    <Stack direction="row" spacing={2}>
+                    <Stack spacing={2} direction="row">
                         <TextField
+                            required
+                            label="First Name"
+                            type="text"
+                            onChange={(event) => setFirstName(event.target.value)}
+                            value={firstName}
+                        />
+                        <TextField
+                            required
+                            label="Last Name"
+                            type="text"
+                            onChange={(event) => setLastName(event.target.value)}
+                            value={lastName}
+                        />
+                    </Stack>
+                    <TextField
+                        required
+                        label="Email"
+                        type="email"
+                        onChange={(event) => setEmail(event.target.value)}
+                        value={email}
+                    />
+                    <TextField
+                        required
+                        label="Password"
+                        type="password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        value={password}
+                    />
+                    <TextField
+                        required
+                        label="Confirm Password"
+                        type="password"
+                        onChange={(event) => setConfirmPassword(event.target.value)}
+                        value={confirmPassword}
+                    />
+                    <Typography variant="h6" color="error">
+                        The following inputs are for development purposes
+                    </Typography>
+                    <Stack spacing={2} direction="row">
+                        <TextField
+                            required
                             label="Role"
                             type="text"
                             onChange={(event) => setRole(event.target.value)}
                             value={role}
                         />
                         <TextField
+                            required
                             label="ID"
                             type="text"
                             onChange={(event) => setID(event.target.value)}
                             value={sait_id}
                         />
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
                         <TextField
-                            label="First name"
-                            type="text"
-                            onChange={(event) => setFirstName(event.target.value)}
-                            value={firstName}
-                        />
-                        <TextField
-                            label="Last name"
-                            type="text"
-                            onChange={(event) => setLastName(event.target.value)}
-                            value={lastName}
-                        />
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <TextField
+                            required
                             label="Code"
                             type="text"
                             onChange={(event) => setCode(event.target.value)}
                             value={secretCode}
                         />
-                        <TextField
-                            label="Email"
-                            type="email"
-                            onChange={(event) => setEmail(event.target.value)}
-                            value={email}
-                        />
                     </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <TextField
-                            label="Password"
-                            type="password"
-                            onChange={(event) => setPassword(event.target.value)}
-                            value={password}
-                        />
-                        <TextField
-                            label="ConfirmPassword"
-                            type="password"
-                            onChange={(event) => setConfirmPassword(event.target.value)}
-                            value={confirmPassword}
-                        />
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <Button type="submit" variant="contained" disabled={isLoading}>
-                            Sign up
+                    <Button type="submit" variant="contained" disabled={isLoading}>
+                        Sign up
+                    </Button>
+                    <Stack direction="row">
+                        <Button variant="text" onClick={() => navigate('/recover')}>
+                            Forgot Password
                         </Button>
-                        <br />
-                        <br />
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <Typography variant="h6">
-                            <Link href="/" underline="hover">
-                                Return to login page
-                            </Link>
-                        </Typography>
+                        <Button variant="text" onClick={() => navigate('/')}>
+                            Back To Home
+                        </Button>
                     </Stack>
                 </Stack>
                 {error && <div className="error">{error}</div>}
             </form>
-        </>
+        </Container>
     );
 };
 
