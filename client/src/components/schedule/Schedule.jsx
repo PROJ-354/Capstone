7b0d5e1766fb3f4932b76abbd217c5c95c91572f
simondumalski <simon@dumalski.com>
2023-03-09 11:09:12 -0700

    Revert "Maybe this fixed it?"
    This reverts commit 7f8c005dfad607f632e3eeabd22708027d9e6f3d.

--- client/src/components/schedule/Schedule.jsx
+++ client/src/components/schedule/Schedule.jsx
@@ -1,48 +1,45 @@
-import {
-    Button,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-    TextField,
-    Typography,
-    useScrollTrigger,
-} from '@mui/material';
-import { Box } from '@mui/system';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Week from './Week';
+<<<<<<< HEAD
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography, useScrollTrigger } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import { useEffect } from "react";
+=======
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+>>>>>>> 87c3b9fdbb54f2fb9edc7b95b43b7888e2b38aa5
+import Week from "./Week";
 
 // test data
 const data = [
     {
-        sunday: { scheduledHours: 1, actualHours: 2 },
-        monday: { scheduledHours: 3, actualHours: 4 },
-        tuesday: { scheduledHours: 5, actualHours: 6 },
-        wednesday: { scheduledHours: 7, actualHours: 8 },
-        thursday: { scheduledHours: 9, actualHours: 10 },
-        friday: { scheduledHours: 11, actualHours: 12 },
-        saturday: { scheduledHours: 13, actualHours: 14 },
+        sunday: { scheduledHours: 1, actualHours: 2, },
+        monday: { scheduledHours: 3, actualHours: 4, },
+        tuesday: { scheduledHours: 5, actualHours: 6, },
+        wednesday: { scheduledHours: 7, actualHours: 8, },
+        thursday: { scheduledHours: 9, actualHours: 10, },
+        friday: { scheduledHours: 11, actualHours: 12, },
+        saturday: { scheduledHours: 13, actualHours: 14, },
     },
     {
-        sunday: { scheduledHours: 0, actualHours: 0 },
-        monday: { scheduledHours: 0, actualHours: 0 },
-        tuesday: { scheduledHours: 2, actualHours: 0 },
-        wednesday: { scheduledHours: 0, actualHours: 0 },
-        thursday: { scheduledHours: 0, actualHours: 0 },
-        friday: { scheduledHours: 0, actualHours: 0 },
-        saturday: { scheduledHours: 0, actualHours: 0 },
+        sunday: { scheduledHours: 0, actualHours: 0, },
+        monday: { scheduledHours: 0, actualHours: 0, },
+        tuesday: { scheduledHours: 2, actualHours: 0, },
+        wednesday: { scheduledHours: 0, actualHours: 0, },
+        thursday: { scheduledHours: 0, actualHours: 0, },
+        friday: { scheduledHours: 0, actualHours: 0, },
+        saturday: { scheduledHours: 0, actualHours: 0, },
     },
-];
+]
 
 /**
  * this component renders a schedule,
  * aka a list of (8) weeks
  */
 const Schedule = () => {
+<<<<<<< HEAD
     //
     const [weeks, setWeeks] = useState(null);
+    
 
     // mreow
     const authenticatedUsersId = JSON.parse(localStorage.getItem('auth')).result._id;
@@ -50,38 +47,41 @@ const Schedule = () => {
     // todo: fetch & render the logged in users schedule
     useEffect(() => {
         async function fn() {
-            const response = await fetch(
-                `http://localhost:42069/api/schedules/${authenticatedUsersId}`
-            );
+            const response = await fetch(`http://localhost:42069/api/schedules/${authenticatedUsersId}`);
             const json = await response.json();
             setWeeks(json[0].weeks);
         }
 
         fn();
     }, []);
+=======
+    // todo: fetch & render the logged in users schedule
+
+>>>>>>> 87c3b9fdbb54f2fb9edc7b95b43b7888e2b38aa5
 
     //
     return (
         <>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 10 }}>
+            <Box sx={{ display: "flex", justifyContent: "center", mt: 10 }}>
                 <Box>
-                    {data &&
-                        data.map((weekData, index) => (
-                            <Week
-                                weekData={weekData}
-                                weekNumber={index + 1}
-                                key={index}
-                            />
-                        ))}
+<<<<<<< HEAD
+                    {weeks && weeks.map((weekData, index) => (
+=======
+                    {data && data.map((weekData, index) => (
+>>>>>>> 87c3b9fdbb54f2fb9edc7b95b43b7888e2b38aa5
+                        <Week weekData={weekData} weekNumber={index + 1} key={index} />
+                    ))}
                 </Box>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', my: 5 }}>
-                <Button variant="contained" color="success">
+            <Box sx={{ display: "flex", justifyContent: "center", my: 5 }}>
+                <Button
+                    variant="contained"
+                    color="success">
                     Submit
                 </Button>
             </Box>
         </>
     );
-};
+}
 
 export default Schedule;
