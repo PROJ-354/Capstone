c419a1fb185292c5d94ba8e1ba72b5179c1deda0
simondumalski <simon@dumalski.com>
2023-03-11 10:27:26 -0700

    Made navbar load right

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,8 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-    const loggedIn = !localStorage.getItem('auth') ? false : true;
+    const loggedIn = localStorage.getItem('auth');
+
+    const location = useLocation();
+    useEffect(() => {}, [location]);
 
     return (
         <>
