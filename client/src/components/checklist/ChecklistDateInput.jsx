36569245b82aad97e721638e0059d45e55b11da8
simondumalski <simon@dumalski.com>
2023-03-14 21:44:43 -0600

    Changed naming for Checklist Input components

---
+++ client/src/components/checklist/ChecklistDateInput.jsx
@@ -0,0 +1,19 @@
+import { TableCell, Input } from '@mui/material';
+import { useState } from 'react';
+
+export default function ChecklistTabPanelDateInput({ sectionName, id, dateValue }) {
+    const [date, setDate] = useState(dateValue == null ? '' : dateValue.split('T')[0]);
+
+    return (
+        <TableCell>
+            <Input
+                type="date"
+                name={`${sectionName} date ${id}`}
+                value={date}
+                onChange={(event) => {
+                    setDate(event.target.value);
+                }}
+            />
+        </TableCell>
+    );
+}
