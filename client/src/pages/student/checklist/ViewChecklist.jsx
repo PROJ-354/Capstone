1166fe4584715c31a41cf876c949abb25f397eb3
simondumalski <simon@dumalski.com>
2023-03-19 11:19:38 -0600

    test commit 2
    Signed-off-by: simondumalski <simon@dumalski.com>

--- client/src/pages/student/checklist/ViewChecklist.jsx
+++ client/src/pages/student/checklist/ViewChecklist.jsx
@@ -28,7 +28,7 @@ export default function ViewChecklist() {
     const handleChange = (isExpanded, panel) => {
         setExpanded(isExpanded ? panel : false);
     };
-    //test commit
+
     //Whether or not the dialog/modal is currently open
     const [open, setOpen] = useState(false);
 
