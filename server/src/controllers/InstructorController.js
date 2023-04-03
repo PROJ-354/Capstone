2f815c41d41fd97083a143f5d981708790120a4e
RyanDelorme <ryan.p.delorme@gmail.com>
2023-04-02 19:28:22 -0600

    Added some functions to generate join codes

--- server/src/controllers/InstructorController.js
+++ server/src/controllers/InstructorController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import JoinCode from '../models/JoinCode.js';
 
 /**
  * Randomly generates a String of a particular length.
@@ -23,13 +24,29 @@ export const generateCode = (length) => {
  */
 export const generateStudentCode = async (request, response, next) => {
     try {
+        // get instructor ID from the front end request
+        const { id } = req.body;
+        
         // generate random 10 character string as code
         const code = generateCode(10);
+
+        // set the role
+        const role = 'student';
+
         // Create expiry date as current date + 7 days
         const currentDate = new Date();
         const expiryDate = new Date();
         expiryDate.setDate(currentDate.getDate() + 7);
 
+        // Create the join code in database
+        const joinCode = await JoinCode.create({
+            code: code,
+            instructorId: id,
+            role: role,
+            expiryDate: expiryDate
+        });
+
+        return res.status(200).json({ result: joinCode });
     } catch (error) {
         next(error);
     }
@@ -37,7 +54,29 @@ export const generateStudentCode = async (request, response, next) => {
 
 export const generatePreceptorCode = async (request, response, next) => {
     try {
+       // get instructor ID from the front end request
+       const { id } = req.body;
+        
+       // generate random 10 character string as code
+       const code = generateCode(10);
 
+       // set the role
+       const role = 'preceptor';
+
+       // Create expiry date as current date + 7 days
+       const currentDate = new Date();
+       const expiryDate = new Date();
+       expiryDate.setDate(currentDate.getDate() + 7);
+
+       // Create the join code in database
+       const joinCode = await JoinCode.create({
+           code: code,
+           instructorId: id,
+           role: role,
+           expiryDate: expiryDate
+       });
+
+       return res.status(200).json({ result: joinCode });
     } catch (error) {
         next(error);
     }
