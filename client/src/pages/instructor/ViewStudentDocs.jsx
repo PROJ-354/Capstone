fd982ebd48b19b68691313168b60c02130652000
Brooks Maclean <90015792+BrooksMac@users.noreply.github.com>
2023-04-13 14:02:49 -0600

    Instructor peval view (#31)
    * INSTRUCTOR VIEW OF EVALUATIONS AND CHECKLISTS
    
    * bug fix to viewing student evaluations

--- client/src/pages/instructor/ViewStudentDocs.jsx
+++ client/src/pages/instructor/ViewStudentDocs.jsx
@@ -56,9 +56,13 @@ export default function ViewStudentDocs() {
                                     container
                                     justifyContent="space-between"
                                     alignItems="center"
-                                ></Grid>
 
-                                <CardHeader
+                                >
+                                    <Typography>{evaluation.student_id}</Typography>
+                                </Grid>
+
+                                 <CardHeader
+
                                     fontWeight="bold"
                                     title="Student Name:"
                                     subheader={
@@ -66,7 +70,9 @@ export default function ViewStudentDocs() {
                                         ' ' +
                                         evaluation.student_id.lastName
                                     }
-                                />
+
+                                /> 
+
                                 <CardContent>
                                     <Typography
                                         sx={{ fontSize: 14 }}
@@ -82,7 +88,9 @@ export default function ViewStudentDocs() {
                                     <CardActions>
                                         <Link
                                             style={{ color: 'white' }}
-                                            to={`/preceptor/${evaluation._id}`}
+
+                                            to={`/instructor/${evaluation.student_id}/peval/${evaluation._id}`}
+
                                         >
                                             <Button variant="contained" color="primary">
                                                 View
@@ -129,8 +137,9 @@ export const studentDocsLoader = async (req) => {
 
     console.log(studentId);
 
-    // NEED TO MAKE A ROUTE TO GRAB EVALS BY STUDENT ID, NOT BY PRECEPTOR ID, MIGHT BE AS SIMPLE AS CHECKING THE USER ROLE AND CHANGING THE KEY TO BE SEARCHED BASED ON THE USER'S ROLE? I DUNNO IM NOT SMORT.
-    const evals = await fetch(`http://localhost:42069/api/preceptor/home/${studentId}`);
+
+    const evals = await fetch(`http://localhost:42069/api/preceptor/studentEvals/${studentId}`);
+
 
     const checks = await fetch(`http://localhost:42069/api/weeks/user/${studentId}`);
 
